ab8a746786f4ac41f31932bbdfa5b62498504bc2
Christopher Phiri <chris7phiri@gmail.com>
2021-12-07 22:31:24 +0200

    can creat json

--- commands/download.js
+++ commands/download.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk')
 const got = require('got');
 const parseHtml = require('../utils/parse');
+const convertTo = require("../utils/convertTo");
 
 async function download() {
     var http = require("http");
@@ -8,7 +9,8 @@ async function download() {
     try {
         const res = await got("https://www.w3schools.com/colors/colors_names.asp");
         var data = parseHtml(res.body);
-        console.log(data);
+        //console.log(data);
+        convertTo("",data);
     } catch (error) {
         console.log(chalk.red(error));
     }
--- utils/convertTo.js
+++ utils/convertTo.js
@@ -1,3 +1,24 @@
+const Handlebars = require("handlebars");
+
+const jsTemplate = `
+const colors = [
+    {{#each colors}}
+        { 
+            name: "{{this.name}}",
+            title: "{{this.title}}",
+            value: "{{this.value}}",
+        },
+    {{/each}}
+];
+
+export default colors;
+`;
+
 function convertTo(type, data = []) {
-    
+    const template = Handlebars.compile(jsTemplate);
+    console.log(template({
+        colors: data
+    }));
 }
+
+module.exports = convertTo;
